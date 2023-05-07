5bfb11ff344cfeba2a42b5681f799d96dd81c35e
shreya kumari <shreya kumari>
2023-05-07 18:16:07 +0530

    added code

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,17 +10,17 @@ const Home = () => {
         <div className='mx-auto max-w-6xl md:px-6 sm:px-6'>
           <div className='grid lg:grid-cols-2 md:grid-rows-2 items-center  gap-8 py-24  md:py-20'>
             <div className='mx-auto max-w-4xl pb-10  gap-8 lg:py-16 md:grid-cols-2 md:py-20'>
-                <h1 className={` font-bold leading-tight mb-6 text-4xl   ${theme === 'light' ? ' text-gray-50' : 'text-black'}`}>
+                <h1 className={` font-bold leading-tight mb-6 text-4xl md:mx-0 mx-1   ${theme === 'light' ? ' text-gray-50' : 'text-black'}`}>
                     Holistic Plans
                 </h1>
                 <div className='mx-auto max-w-3xl'>
-                  <p className={ `mb-8 font-medium  sm:text-xl md:text-2xl  ${theme === 'light' ? ' text-gray-100' : 'text-black'}`}>Dedicated pediatrician for your baby, personal care plan, Best possible baby care</p>
-                   <div className='flex max-w-none gap-4 pt-20'>
-                    <button className='flex w-full sm:w-auto'>
-                        <a className='w-full text-base font-medium sm:text-lg md:text-xl bg-blue-600 hover:bg-green-950 py-4 px-6 rounded-md text-white '>Get Started</a>
+                  <p className={ `mb-8 font-medium  sm:text-xl md:text-2xl md:mx-0 mx-1  ${theme === 'light' ? ' text-gray-100' : 'text-black'}`}>Dedicated pediatrician for your baby, personal care plan, Best possible baby care</p>
+                   <div className='flex max-w-none pt-20 md:gap-4 gap-1'>
+                    <button className='flex w-full sm:w-auto '>
+                        <a className='w-full text-base font-medium sm:text-lg md:text-xl bg-blue-600 mx-1 hover:bg-green-950 py-4 px-6 rounded-md text-white '>Get Started</a>
                     </button>
                     <button className='flex w-full sm:w-auto'>
-                        <a className='w-full text-base border-yellow-500 bg-yellow-600 text-white py-4 hover:bg-yellow-950 px-12 rounded-md font-medium sm:text-lg md:text-xl'>Preview Access</a>
+                        <a className='w-full text-base border-yellow-500 bg-yellow-600 text-white py-4 hover:bg-yellow-950 md:px-12 px-6 mx-1 rounded-md font-medium sm:text-lg md:text-xl'>Preview Access</a>
                     </button>
                    </div>
                 </div>
